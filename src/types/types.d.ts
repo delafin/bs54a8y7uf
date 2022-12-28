14164819ac6def6dbd48612f2448bef7ae841a48
delafin <lifinhime@gmail.com>
2022-12-28 11:54:05 +0200

    global fixes

---
+++ src/types/types.d.ts
@@ -0,0 +1,27 @@
+type Children = {
+    children:React.ReactNode
+};
+type Users = {
+    id:number;
+    name:string;
+    email:string;
+    phone:string;
+    position:string;
+    position_id:number;
+    registration_timestamp:number;
+    photo:string;
+}
+type Links = {
+    next_url:string;
+    prev_url?:string|null;
+};
+type UserList = {
+    success:boolean;
+    total_pages:number;
+    total_users:number;
+    count:number;
+    page:number;
+    links:Links;
+    users: Users[];
+}
+// JSON.parse(JSON.stringify(member))
